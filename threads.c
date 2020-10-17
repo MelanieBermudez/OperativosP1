5a2c01f294238ee58a1f1c1b1e8d31f235c66a99
Melanie Bermudez <mebeca19@gmail.com>
2020-10-16 20:38:15 -0600

    prueba de threads funcional

--- threads.c
+++ threads.c
@@ -5,9 +5,25 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
+void * myturn(void * arg){
+	while(1){
+		sleep(1);
+		printf("My turn \n");
+	}
+	return NULL;
+}
+void yourturn(){
+	while(1){
+		sleep(2);
+		printf("your turn \n");
+	}
+}
 int main(int argc, char*argv[])
 {
+	pthread_t newthread;
+	pthread_create(&newthread,NULL,myturn,NULL);
 	
+	//myturn();
+	yourturn();
 	return 0;
 }
